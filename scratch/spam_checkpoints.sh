6fe55a000e8f0d7f0efb77e955270d85ae306754
Sumner Evans <sumner@beeper.com>
2023-05-15 10:20:40 -0600

    scratch: add spam_checkpoints script
    Signed-off-by: Sumner Evans <sumner@beeper.com>

---
+++ scratch/spam_checkpoints.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for i in {1..105}; do
+    data='{"checkpoints": [{"event_type":"m.room.message","event_id": "$test'
+    data="$data$i\",\"room_id\":\"!test:foo\",\"step\":\"BRIDGE\",\"status\":\"SUCCESS\", \"reported_by\":\"BRIDGE\"}]}"
+
+    curl http://localhost:8585/v1/send_message_checkpoints/sumner/slack \
+        -X POST \
+        --data "$data"
+done
